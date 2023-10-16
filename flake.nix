1eb87dd2f8a11a646bd1cdcdebc68fb3c932e9cd
Toby Selway <tobyselway@outlook.com>
2023-10-16 23:16:50 +0100

    feat: 3D cube on plane

--- flake.nix
+++ flake.nix
@@ -17,6 +17,7 @@
             udev alsa-lib vulkan-loader
             xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
             libxkbcommon wayland # To use the wayland feature
+            clang mold # fast linker
         ];
         LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
     };
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,47 @@
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0)
+            .looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
-    }
-}
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app
-            .add_systems(Startup, add_people)
-            .add_systems(Update, greet_people);
-    }
+fn spawn_basic_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0, subdivisions: 0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.67, 0.84, 0.3).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
 }
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
         .add_plugins((
-            DefaultPlugins,
-            HelloPlugin,
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Porridge".to_string(),
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        ))
+        .add_systems(Startup, (
+            spawn_basic_scene,
+            spawn_camera,
         ))
         .run();
 }
