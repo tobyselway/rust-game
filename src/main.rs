89cc89a309b36bcad61dd62146a476fa2028c971
Toby Selway <tobyselway@outlook.com>
2023-10-16 18:27:25 +0100

    feat: created system

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use bevy::prelude::App;
+use bevy::prelude::{App, Update};
+
+fn hello_world() {
+    println!("hello world!");
+}
 
 fn main() {
-    App::new().run();
-    println!("Hello");
+    App::new()
+        .add_systems(Update, hello_world)
+        .run();
 }
