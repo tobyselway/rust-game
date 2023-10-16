e671b189b07dd77b07cd4426eaf18d1c45d7eafd
Toby Selway <tobyselway@outlook.com>
2023-10-16 18:36:55 +0100

    feat: using ECS

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
-use bevy::prelude::{App, Update};
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
 
 fn hello_world() {
     println!("hello world!");
@@ -6,6 +24,10 @@ fn hello_world() {
 
 fn main() {
     App::new()
-        .add_systems(Update, hello_world)
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (
+            hello_world,
+            greet_people,
+        ))
         .run();
 }
