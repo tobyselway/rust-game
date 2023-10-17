c313b0a736ee775c7e28fb3962d48f97c4529352
Toby Selway <tobyselway@outlook.com>
2023-10-17 06:42:25 +0100

    feat: raycasting & spawnable towers

--- src/camera.rs
+++ src/camera.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-
+use bevy_mod_picking::prelude::RaycastPickCamera;
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
@@ -11,11 +11,13 @@ impl Plugin for CameraPlugin {
 }
 
 fn spawn_camera(mut commands: Commands) {
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0)
-            .looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
+    commands
+        .spawn(Camera3dBundle {
+            transform: Transform::from_xyz(-2.0, 2.5, 5.0)
+                .looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        })
+        .insert(RaycastPickCamera::default());
 }
 
 fn camera_controls(
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use bevy::{
     prelude::*,
     window::WindowResolution,
+    pbr::NotShadowCaster,
+    math::vec4,
 };
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_mod_picking::{
+    *,
+    prelude::{ Highlight, HighlightKind, RaycastPickTarget, DebugPickingPlugin },
+};
 
 use camera::*;
 use target::*;
@@ -17,11 +23,15 @@ mod tower;
 #[derive(Resource)]
 pub struct GameAssets {
     bullet_scene: Handle<Scene>,
+    tower_base_scene: Handle<Scene>,
+    tomato_tower_scene: Handle<Scene>,
 }
 
 fn asset_loading(mut commands: Commands, assets: Res<AssetServer>) {
     commands.insert_resource(GameAssets {
         bullet_scene: assets.load("Bullet.glb#Scene0"),
+        tower_base_scene: assets.load("TowerBase.glb#Scene0"),
+        tomato_tower_scene: assets.load("TomatoTower.glb#Scene0"),
     });
 }
 
@@ -29,7 +39,24 @@ fn spawn_basic_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    assets: Res<GameAssets>,
 ) {
+    let default_collider_color = materials.add(Color::rgba(0.3, 0.5, 0.3, 0.3).into());
+    const HIGHLIGHT_TINT: Highlight<StandardMaterial> = Highlight {
+        hovered: Some(HighlightKind::new_dynamic(|matl| StandardMaterial {
+            base_color: matl.base_color + vec4(-0.2, -0.2, 0.4, 0.0),
+            ..matl.to_owned()
+        })),
+        pressed: Some(HighlightKind::new_dynamic(|matl| StandardMaterial {
+            base_color: matl.base_color + vec4(-0.3, -0.3, 0.5, 0.0),
+            ..matl.to_owned()
+        })),
+        selected: Some(HighlightKind::new_dynamic(|matl| StandardMaterial {
+            base_color: matl.base_color + vec4(-0.3, 0.2, -0.3, 0.0),
+            ..matl.to_owned()
+        })),
+    };
+
     commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Plane { size: 25.0, subdivisions: 0 })),
         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
@@ -37,17 +64,21 @@ fn spawn_basic_scene(
     })
     .insert(Name::new("Ground"));
 
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.67, 0.84, 0.3).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    })
-    .insert(Tower {
-        shooting_timer: Timer::from_seconds(1.0, TimerMode::Repeating),
-        bullet_offset: Vec3::new(0.0, 0.2, 0.5),
-    })
-    .insert(Name::new("Tower"));
+    commands.spawn(SpatialBundle::from_transform(Transform::from_xyz(0.0, 0.8, 0.0)))
+        .insert(Name::new("Tower_Base"))
+        .insert(meshes.add(shape::Capsule::default().into()))
+        .insert(default_collider_color)
+        .insert(HIGHLIGHT_TINT)
+        .insert(NotShadowCaster)
+        .insert(PickableBundle::default())
+        .insert(RaycastPickTarget::default())
+        .with_children(|commands| {
+            commands.spawn(SceneBundle {
+                scene: assets.tower_base_scene.clone(),
+                transform: Transform::from_xyz(0.0, -0.8, 0.0),
+                ..default()
+            });
+        });
 
     commands.spawn(PointLightBundle {
         point_light: PointLight {
@@ -88,14 +119,19 @@ fn main() {
                     ..default()
                 }),
             WorldInspectorPlugin::new(),
+            DefaultPickingPlugins
+                .build()
+                .disable::<DebugPickingPlugin>(),
             CameraPlugin,
             BulletPlugin,
             TargetPlugin,
             TowerPlugin,
         ))
         // Systems
-        .add_systems(Startup, (
+        .add_systems(PreStartup, (
             asset_loading,
+        ))
+        .add_systems(Startup, (
             spawn_basic_scene,
         ))
         .run();
