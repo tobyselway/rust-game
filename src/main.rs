1a5639e51315a0c95987db5b6e119a2d3ef4a30e
Toby Selway <tobyselway@outlook.com>
2023-10-17 01:57:00 +0100

    feat: inspector & spawning + despawning bullets

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-use bevy::prelude::*;
+use std::f32::consts::PI;
+
+use bevy::{
+    prelude::*,
+    window::WindowResolution
+};
+
+use bevy::ecs::component::Component;
+
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera3dBundle {
@@ -6,6 +15,59 @@ fn spawn_camera(mut commands: Commands) {
             .looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
+}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Lifetime {
+    timer: Timer,
+}
+
+fn bullet_despawn(
+    mut commands: Commands,
+    mut bullets: Query<(Entity, &mut Lifetime)>,
+    time: Res<Time>,
+) {
+    for (entity, mut lifetime) in &mut bullets {
+        lifetime.timer.tick(time.delta());
+        if lifetime.timer.just_finished() {
+            commands.entity(entity).despawn_recursive();
+        }
+    }
+}
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+pub struct Tower {
+    shooting_timer: Timer,
+}
+
+fn tower_shooting(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut towers: Query<&mut Tower>,
+    time: Res<Time>,
+) {
+    for mut tower in &mut towers {
+        tower.shooting_timer.tick(time.delta());
+        if tower.shooting_timer.just_finished() {
+            let spawn_transform = Transform::from_xyz(0.0, 0.7, 0.6)
+                .with_rotation(Quat::from_rotation_y(-PI / 2.0));
+
+            commands
+                .spawn(PbrBundle {
+                    mesh: meshes.add(Mesh::from(shape::Cube { size: 0.1 })),
+                    material: materials.add(Color::rgb(0.87, 0.44, 0.42).into()),
+                    transform: spawn_transform,
+                    ..default()
+                })
+                .insert(Lifetime {
+                    timer: Timer::from_seconds(0.5, TimerMode::Once),
+                })
+                .insert(Name::new("Bullet"));
+        }
+    }
 }
 
 fn spawn_basic_scene(
@@ -17,13 +79,30 @@ fn spawn_basic_scene(
         mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0, subdivisions: 0 })),
         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         ..default()
-    });
+    })
+    .insert(Name::new("Ground"));
+
     commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
         material: materials.add(Color::rgb(0.67, 0.84, 0.3).into()),
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..default()
-    });
+    })
+    .insert(Tower {
+        shooting_timer: Timer::from_seconds(1.0, TimerMode::Repeating),
+    })
+    .insert(Name::new("Tower"));
+
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    })
+    .insert(Name::new("Light"));
 }
 
 fn main() {
@@ -33,15 +112,24 @@ fn main() {
             DefaultPlugins
                 .set(WindowPlugin {
                     primary_window: Some(Window {
-                        title: "Porridge".to_string(),
+                        title: "Porridge".to_string(),                        
+                        resolution: WindowResolution::new(600.0, 400.0)
+                            .with_scale_factor_override(1.0),
                         ..default()
                     }),
                     ..default()
                 }),
+            WorldInspectorPlugin::new(),
         ))
+        .register_type::<Tower>()
+        // Systems
         .add_systems(Startup, (
             spawn_basic_scene,
             spawn_camera,
+        ))
+        .add_systems(Update, (
+            tower_shooting,
+            bullet_despawn,
         ))
         .run();
 }
